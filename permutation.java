765c41525a8d93901ca1f63ce91197da36b85e5f
Wxinm <wxinm2016@sina.com>
2017-03-06 20:29:00 +0800

    updated in 20170306 by Wxinm

--- permutation.java
+++ permutation.java
@@ -1,13 +1,66 @@
-/*
-编写一个程序，输出字符串"abcdefgh"的全排列。（提示：可考虑递归）
-*/
+package edu.develop.homework;
+
+/**
+ * @title 全排列
+ * @author 王馨漫
+ * @description 编写一个程序，输出字符串"abcdefgh"的全排列。
+ */
+
 public class permutation{
+	
 	public static void main(String[] args){
-		char m;
-		char[] chars={'a','b','c'};
-		
-                //打印chars的全排列		
-		permutate(chars,0);
+		char[] chars={'a','b','c','d','e'};
+		int lineNum = factorial(chars.length-1);
+		permutate(chars, 0, lineNum);
+	}
+	
+	/**
+	 * 全排序
+	 * @param chars char型数组
+	 * @param start 开始索引
+	 */
+	private static int count = 0;
+	public static void permutate(char[] chars, int start, int lineNum){
+		if(start == chars.length){
+			System.out.print(String.valueOf(chars));
+			System.out.print(" ");
+			count++;
+			if(count%lineNum == 0)
+				System.out.println();
+		}
+		else{
+			for(int i = start; i < chars.length; i++){
+				swap(chars, start, i);
+				permutate(chars, start+1, lineNum);
+				swap(chars, start, i);
+			}
+		}
+	}
+	
+	/**
+	 * 数组中任意两个元素交换
+	 * @param chars char型数组
+	 * @param a 元素索引
+	 * @param b 元素索引
+	 */
+	private static void swap(char[] chars, int a, int b){
+		char temp = chars[a];
+		chars[a] = chars[b];
+		chars[b] = temp;
+	}
+	
+	/**
+	 * @param n
+	 * @return n阶乘
+	 */
+	private static int factorial(int n){
+		if(n < 0)
+			return -1;
+		if(n == 0 || n==1)
+			return 1;
+		else{
+			return n * factorial(n-1);
+		}
 	}
 			
 }
--- sort.java
+++ sort.java
@@ -1,15 +1,40 @@
-/*
-用Java编写一个能对数组比较的程序，需要输出按行升序排序的结果 如输入
-1 2 5 4 8
-输出：
-1 2 4 5 8
-要求：数组是通过sort程序的参数输入。
-*/
+package edu.develop.homework;
+
+import java.util.Arrays;
+
+/**
+ * @title 排列
+ * @author 王馨漫
+ * @description 按升序排序，输入为main函数参数
+ */
 public class sort{
 	 public static void main(String[] args){
-         //将args转换为int数组
-         
-         //对int数组排序后输出结果
+		 
+		 int[] sorted = new int[args.length];
+		 
+		 //String数组转int数组
+		 int i = 0;
+		 for(String para:args){
+			 try { 
+		         sorted[i++] = Integer.parseInt(para);  
+		     } catch (NumberFormatException e) {  
+		         System.out.println("输入非数字,程序退出");
+		         System.exit(1);
+		     } 
+		 }
+		 
+		 //冒泡排序
+		 for(i = 0; i < sorted.length-1; i++){
+			 for(int j = 0; j < sorted.length-1-i; j++){
+				 if(sorted[j] > sorted[j+1]){
+					 int temp = sorted[j];
+					 sorted[j] = sorted[j+1];
+					 sorted[j+1] = temp;
+				 }
+			 }
+		 }
 	 	 
+		 System.out.println("Bubble Sort:"+Arrays.toString(sorted));
+		 
 	}
 }
