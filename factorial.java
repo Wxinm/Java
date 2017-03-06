589ec55889c4710af9398d033d5c75d17940aad9
Wxinm <wxinm2016@sina.com>
2017-03-07 00:03:25 +0800

    factorial updated

--- factorial.java
+++ factorial.java
@@ -1,11 +1,43 @@
+package edu.develop.homework;
+
 /*
 编写一个程序，计算自然数n的阶乘n!。（提示：可用循环完成）
 */
 public class factorial{
+	
+	/**
+	 * @param n
+	 * @return n阶乘
+	 */
+	private static int factorial(int n){
+		if(n < 0)
+			return -1;
+		if(n == 0 || n==1)
+			return 1;
+		else{
+			return n * factorial(n-1);
+		}
+	}
+	
 	 public static void main(String[] args){
-         //将args[0]转换为阶数n
-         
-         //循环计算n!，返回结果
+		 
+		 if(args.length < 1){
+			 System.out.println("请传参输入n");
+		 }
+		 else{
+			 try { 
+		         int n = Integer.parseInt(args[0]);  
+		         if(n < 0)
+		        	 System.out.println("请输入正整数");
+		         else
+		        	 System.out.println(factorial(n));
+		     } catch (NumberFormatException e) {  
+		         System.out.println("请输入整数");
+		     } 
+			 
+		 }
+		 
+		 
 	 	 
 	}
 }
